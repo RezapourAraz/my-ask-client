04d8ebdcca378f0051a2f41893be349ebe68630d
ArazRezapour <rezapouraraz@gmail.com>
2023-12-22 10:28:53 +0330

    title added on login and register pages

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -13,91 +13,99 @@ import {
 
 // Icons
 import { FaUser } from "react-icons/fa";
+import Head from "next/head";
 
 const Login = () => {
   return (
-    <Grid
-      container
-      sx={{
-        height: "100vh",
-        bgcolor: "secondary.main",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="Travel App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Grid
+        container
         sx={{
-          width: 400,
-          border: 1,
-          px: 2,
-          py: 5,
-          borderRadius: 2,
-          borderColor: "grey.400",
+          height: "100vh",
+          bgcolor: "secondary.main",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <Box sx={{ textAlign: "center" }}>
-          <Typography variant="h2">Login</Typography>
-        </Box>
-        <Grid my={2} sx={{ textAlign: "center" }}>
-          <Typography variant="h5">welcome back!</Typography>
-        </Grid>
-        <Grid my={3}>
-          <Input
-            sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
-            fullWidth
-            placeholder="Email/Username"
-            startAdornment={
-              <InputAdornment position="start">
-                <FaUser />
-              </InputAdornment>
-            }
-          />
-        </Grid>
-        <Grid mt={3}>
-          <Input
-            sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
-            fullWidth
-            placeholder="Password"
-            type="password"
-            startAdornment={
-              <InputAdornment position="start">
-                <FaUser />
-              </InputAdornment>
-            }
-          />
-        </Grid>
         <Grid
-          my={2}
-          container
-          sx={{ alignItems: "center", justifyContent: "space-between" }}
+          sx={{
+            width: 400,
+            border: 1,
+            px: 2,
+            py: 5,
+            borderRadius: 2,
+            borderColor: "grey.400",
+          }}
         >
-          <Typography variant="h6">
-            Have an account?{" "}
-            <Typography
-              variant="h6"
-              component="span"
-              sx={{ color: "primary.main" }}
-            >
-              login
-            </Typography>
-          </Typography>
-          <Typography variant="h6">Forgot Password?</Typography>
-        </Grid>
-        <Grid container my={1} sx={{ alignItems: "center" }}>
-          <Checkbox />
-          <Typography variant="h6">Remember me</Typography>
-        </Grid>
-        <Grid>
-          <Button
-            variant="contained"
-            fullWidth
-            sx={{ boxShadow: 0, color: "common.white" }}
+          <Box sx={{ textAlign: "center" }}>
+            <Typography variant="h2">Login</Typography>
+          </Box>
+          <Grid my={2} sx={{ textAlign: "center" }}>
+            <Typography variant="h5">welcome back!</Typography>
+          </Grid>
+          <Grid my={3}>
+            <Input
+              sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
+              fullWidth
+              placeholder="Email/Username"
+              startAdornment={
+                <InputAdornment position="start">
+                  <FaUser />
+                </InputAdornment>
+              }
+            />
+          </Grid>
+          <Grid mt={3}>
+            <Input
+              sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
+              fullWidth
+              placeholder="Password"
+              type="password"
+              startAdornment={
+                <InputAdornment position="start">
+                  <FaUser />
+                </InputAdornment>
+              }
+            />
+          </Grid>
+          <Grid
+            my={2}
+            container
+            sx={{ alignItems: "center", justifyContent: "space-between" }}
           >
-            Log in
-          </Button>
+            <Typography variant="h6">
+              Have an account?{" "}
+              <Typography
+                variant="h6"
+                component="span"
+                sx={{ color: "primary.main" }}
+              >
+                login
+              </Typography>
+            </Typography>
+            <Typography variant="h6">Forgot Password?</Typography>
+          </Grid>
+          <Grid container my={1} sx={{ alignItems: "center" }}>
+            <Checkbox />
+            <Typography variant="h6">Remember me</Typography>
+          </Grid>
+          <Grid>
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{ boxShadow: 0, color: "common.white" }}
+            >
+              Log in
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
--- pages/auth/register/index.tsx
+++ pages/auth/register/index.tsx
@@ -15,107 +15,114 @@ import {
 import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
+import Head from "next/head";
 
 const Register = () => {
   return (
-    <Grid
-      container
-      sx={{
-        height: "100vh",
-        bgcolor: "secondary.main",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <>
+      <Head>
+        <title>Register</title>
+        <meta name="description" content="Travel App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Grid
+        container
         sx={{
-          width: 400,
-          border: 1,
-          px: 2,
-          py: 5,
-          borderRadius: 2,
-          borderColor: "grey.400",
+          height: "100vh",
+          bgcolor: "secondary.main",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <Box sx={{ textAlign: "center" }}>
-          <Typography variant="h2">Create Account</Typography>
-        </Box>
-        <Grid my={2} sx={{ textAlign: "center" }}>
-          <Typography variant="h5">welcome to My Ask</Typography>
-        </Grid>
-        <Grid my={3}>
-          <Input
-            sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
-            fullWidth
-            placeholder="Username*"
-            startAdornment={
-              <InputAdornment position="start">
-                <FaUser />
-              </InputAdornment>
-            }
-          />
-        </Grid>
-        <Grid my={3}>
-          <Input
-            sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
-            fullWidth
-            placeholder="Email*"
-            startAdornment={
-              <InputAdornment position="start">
-                <MdEmail />
-              </InputAdornment>
-            }
-          />
-        </Grid>
-        <Grid my={3}>
-          <Input
-            sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
-            fullWidth
-            placeholder="Password*"
-            startAdornment={
-              <InputAdornment position="start">
-                <RiLockPasswordFill />
-              </InputAdornment>
-            }
-          />
-        </Grid>
-        <Grid mt={3}>
-          <Input
-            sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
-            fullWidth
-            placeholder="Confirm Password*"
-            type="password"
-            startAdornment={
-              <InputAdornment position="start">
-                <RiLockPasswordFill />
-              </InputAdornment>
-            }
-          />
-        </Grid>
-        <Grid my={2} container sx={{ alignItems: "center" }}>
-          <Typography variant="h6">
-            Have an account?{" "}
-            <Typography
-              variant="h6"
-              component="span"
-              sx={{ color: "primary.main" }}
-            >
-              login
+        <Grid
+          sx={{
+            width: 400,
+            border: 1,
+            px: 2,
+            py: 5,
+            borderRadius: 2,
+            borderColor: "grey.400",
+          }}
+        >
+          <Box sx={{ textAlign: "center" }}>
+            <Typography variant="h2">Create Account</Typography>
+          </Box>
+          <Grid my={2} sx={{ textAlign: "center" }}>
+            <Typography variant="h5">welcome to My Ask</Typography>
+          </Grid>
+          <Grid my={3}>
+            <Input
+              sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
+              fullWidth
+              placeholder="Username*"
+              startAdornment={
+                <InputAdornment position="start">
+                  <FaUser />
+                </InputAdornment>
+              }
+            />
+          </Grid>
+          <Grid my={3}>
+            <Input
+              sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
+              fullWidth
+              placeholder="Email*"
+              startAdornment={
+                <InputAdornment position="start">
+                  <MdEmail />
+                </InputAdornment>
+              }
+            />
+          </Grid>
+          <Grid my={3}>
+            <Input
+              sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
+              fullWidth
+              placeholder="Password*"
+              startAdornment={
+                <InputAdornment position="start">
+                  <RiLockPasswordFill />
+                </InputAdornment>
+              }
+            />
+          </Grid>
+          <Grid mt={3}>
+            <Input
+              sx={{ bgcolor: "grey.800", borderRadius: 1, px: 1, py: 0.5 }}
+              fullWidth
+              placeholder="Confirm Password*"
+              type="password"
+              startAdornment={
+                <InputAdornment position="start">
+                  <RiLockPasswordFill />
+                </InputAdornment>
+              }
+            />
+          </Grid>
+          <Grid my={2} container sx={{ alignItems: "center" }}>
+            <Typography variant="h6">
+              Have an account?{" "}
+              <Typography
+                variant="h6"
+                component="span"
+                sx={{ color: "primary.main" }}
+              >
+                login
+              </Typography>
             </Typography>
-          </Typography>
-        </Grid>
-
-        <Grid>
-          <Button
-            variant="contained"
-            fullWidth
-            sx={{ boxShadow: 0, color: "common.white" }}
-          >
-            Signup
-          </Button>
+          </Grid>
+          <Grid>
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{ boxShadow: 0, color: "common.white" }}
+            >
+              Signup
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
